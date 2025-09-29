68c6c2a89de572bf72882a9aae371ed29b433982
THIAGO 360 GRAUS <108551126+PASSEIOVIRTUAL360@users.noreply.github.com>
2025-09-28 22:08:40 -0300

    6COM

--- skin.js
+++ skin.js
@@ -1,7 +1,7 @@
 // Garden Gnome Software - Skin
 // Pano2VR 7.1.10/21009
 // Filename: XVCVXVC.ggsk
-// Generated 2025-09-28T21:37:04
+// Generated 2025-09-28T22:08:38
 
 function pano2vrSkin(player,base) {
 	player.addVariable('opt_3d_preview', 2, true, { ignoreInState: 1  });
